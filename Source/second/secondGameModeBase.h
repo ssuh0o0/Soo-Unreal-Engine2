79480d50b5a8d3f6e9264f6f3e50eb7ef91d3b2b
ssuh0o0 <ssuh0o0@daum.net>
2021-04-23 16:11:33 +0900

    commit

---
+++ Source/second/secondGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "secondGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SECOND_API AsecondGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
