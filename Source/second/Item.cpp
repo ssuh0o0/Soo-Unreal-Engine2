54b8a6173ebd0020dca86b16bc571266bf411957
ssuh0o0 <ssuh0o0@daum.net>
2021-05-27 17:14:32 +0900

    make son class of item

--- Source/second/Item.cpp
+++ Source/second/Item.cpp
@@ -33,11 +33,11 @@ void AItem::Tick(float DeltaTime)
 
 void AItem::OnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult)
 {
-	UE_LOG(LogTemp, Warning, TEXT("overlap begin"));
+	UE_LOG(LogTemp, Warning, TEXT("Super::overlap begin"));
 }
 
 void AItem::OnOverlapEnd(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
 {
-	UE_LOG(LogTemp, Warning, TEXT("overlap end"));
+	UE_LOG(LogTemp, Warning, TEXT("Super::overlap end"));
 }
 
